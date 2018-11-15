c3e85aa43957d9e35a56985e6128673f113912e0
peisuke <peisuke.com@gmail.com>
2018-11-15 05:27:36 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -204,7 +204,34 @@ function install_cudnn_7 () {
 
 function install_chainer_5_0_0 () {
     cd $SETUP_INSTALL_DIR
-    sh ./chainer_setup.sh
+
+    mkdir chainer_setup
+    cd chainer_setup
+    wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.3.tar.gz
+    tar xzvf openmpi-3.1.3.tar.gz
+    rm openmpi-3.1.3.tar.gz
+    cd openmpi-3.1.3
+    ./configure --with-cuda
+    make -j4
+    sudo make install
+    cd ..
+    
+    git clone -b v2.3.7-1 https://github.com/NVIDIA/nccl.git
+    cd nccl
+    make -j src.build
+    make pkg.debian.build
+    sudo apt -y install build-essential devscripts debhelper
+    make pkg.debian.build
+    sudo dpkg -i build/pkg/deb/libnccl*
+    cd ..
+    
+    # install python by using pyenv
+    export PYENV_PYTHON_VERSION="3.6.1"
+    env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYENV_PYTHON_VERSION
+    pyenv global $PYENV_PYTHON_VERSION
+    
+    pip install mpi4py==3.0.0
+    pip install chainer==5.0.0 cupy==5.0.0
 }
 
 echo -n "Set up first? [Y/n] "
